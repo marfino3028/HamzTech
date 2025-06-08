b63b9730404d235fb5e6b7a9852c50e2119e8bbe
marfinohamzah455 <marfinohamzah455@gmail.com>
2025-06-08 10:51:45 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true, // Penting untuk cache-busting
+  }
 });
