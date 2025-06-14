cbc0d28d0e1ca6cbd90019732e5966f42cb06b56
marfinohamzah455 <marfinohamzah455@gmail.com>
2025-06-14 16:24:15 +0700

    ignore update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
-// tailwind.config.js
-content: [
-  "./resources/**/*.blade.php",
-  "./resources/**/*.js",
-  "./resources/**/*.vue",
-]
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+            },
+        },
+    },
+    plugins: [],
+};
