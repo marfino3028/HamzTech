adf2e73a0071bf3b69ee1bed67c00a75511f729b
marfinohamzah455 <marfinohamzah455@gmail.com>
2025-06-16 15:11:21 +0700

    first init

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,10 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    build: {
-    manifest: true,
-    outDir: 'public/build',
-  },
 });
