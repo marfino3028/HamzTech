a66861ac87e6252db436de450305051bf3b97f6a
marfinohamzah455 <marfinohamzah455@gmail.com>
2025-06-17 13:09:48 +0700

    Add GitHub Actions for Koyeb

--- storage/framework/views/ce1cc08c95e00040ed493c64a31f63e1.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<h1 class="text-3xl font-bold mb-4">Afiliasi dan Rekomendasi Produk</h1>
-<p>Saya telah mengkurasi berbagai produk terbaik seputar teknologi, laptop, dan perlengkapan digital. Kamu bisa lihat semuanya di:</p>
-
-<a href="https://lynk.id/marfino3028" target="_blank" class="inline-block mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-    Kunjungi Lynk.id Saya
-</a>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/freelance/portofolio-it/resources/views/affiliate.blade.php ENDPATH**/ ?>
